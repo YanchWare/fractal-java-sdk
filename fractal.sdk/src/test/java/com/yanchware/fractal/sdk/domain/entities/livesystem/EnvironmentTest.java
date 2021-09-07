7d95db5891cccdb218d0357dee54f121250f47ad
Flavian Popa <fp@yanchware.com>
2021-09-07 15:41:17 +0300

    FRA-4 Refactor tests

--- fractal.sdk/src/test/java/com/yanchware/fractal/sdk/domain/entities/livesystem/EnvironmentTest.java
+++ fractal.sdk/src/test/java/com/yanchware/fractal/sdk/domain/entities/livesystem/EnvironmentTest.java
@@ -9,29 +9,29 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class EnvironmentTest {
 
     @Test
-    public void when_EnvironmentCreatedWithNullId_ExceptionThrown() {
-        assertThatThrownBy(() -> generateBuilderWithInfo().build()).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("Environment id has not been defined");
+    public void exceptionThrown_when_environmentCreatedWithNullId() {
+        assertThatThrownBy(() -> generateBuilderWithInfo(null).build()).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("Environment id has not been defined");
     }
 
     @Test
-    public void when_EnvironmentCreatedWithEmptyId_ExceptionThrown() {
-        assertThatThrownBy(() -> generateBuilderWithInfo().build()).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("Environment id has not been defined");
+    public void exceptionThrown_when_environmentCreatedWithEmptyId() {
+        assertThatThrownBy(() -> generateBuilderWithInfo("").build()).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("Environment id has not been defined");
     }
 
     @Test
-    public void when_EnvironmentCreatedWithBlankId_ExceptionThrown() {
-        assertThatThrownBy(() -> generateBuilderWithInfo().build()).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("Environment id has not been defined");
+    public void exceptionThrown_when_environmentCreatedWithBlankId() {
+        assertThatThrownBy(() -> generateBuilderWithInfo("   ").build()).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("Environment id has not been defined");
     }
 
     @Test
-    public void when_EnvironmentCreatedWithValidId_NoValidationErrors() {
-        var env = generateBuilderWithInfo().id("production-001").build();
+    public void noValidationErrors_when_environmentCreatedWithValidId() {
+        var env = generateBuilderWithInfo("production-001").build();
         assertThat(env.validate()).isEmpty();
     }
 
-    private Environment.EnvironmentBuilder generateBuilderWithInfo() {
+    private Environment.EnvironmentBuilder generateBuilderWithInfo(String id) {
         return Environment.builder()
-                .id("    ")
+                .id(id)
                 .displayName("PROD")
                 .parentId("123456789")
                 .parentType("folder");
--- fractal.sdk/src/test/java/com/yanchware/fractal/sdk/services/LiveSystemServiceTest.java
+++ fractal.sdk/src/test/java/com/yanchware/fractal/sdk/services/LiveSystemServiceTest.java
@@ -1,16 +1,48 @@
 package com.yanchware.fractal.sdk.services;
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.yanchware.fractal.sdk.configuration.SdkConfiguration;
 import com.yanchware.fractal.sdk.domain.exceptions.InstantiatorException;
 import com.yanchware.fractal.sdk.services.contracts.livesystemcontract.commands.InstantiateLiveSystemCommandRequest;
 import com.yanchware.fractal.sdk.services.contracts.livesystemcontract.dtos.EnvironmentDto;
+import com.yanchware.fractal.sdk.utils.LocalSdkConfiguration;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+
+import java.net.http.HttpClient;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 public class LiveSystemServiceTest {
 
-    private LiveSystemService liveSystemService = new LiveSystemService(null, null);
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(8090);
+
+    private LiveSystemService liveSystemService;
+
+    @Before
+    public void setUp() {
+        SdkConfiguration sdkConfiguration = new LocalSdkConfiguration();
+        HttpClient httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .build();
+        liveSystemService = new LiveSystemService(httpClient, sdkConfiguration);
+    }
 
     @Test
-    public void test() throws InstantiatorException {
+    public void urlPathMatching_when_postRequestToLiveSystem() throws InstantiatorException {
+        stubFor(post(urlPathMatching("/livesystem/resource-group/livesystems"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")));
+
+        liveSystemService.instantiate(buildLiveSystemCommand());
+
+        verify(postRequestedFor(urlPathEqualTo("/livesystem/resource-group/livesystems")));
+    }
+
+    private InstantiateLiveSystemCommandRequest buildLiveSystemCommand() {
         InstantiateLiveSystemCommandRequest command = InstantiateLiveSystemCommandRequest.builder()
                 .liveSystemId("resourceGroupId/prod") //form of "resourceGroupId/name"
                 .description("prod")
@@ -20,9 +52,9 @@ public class LiveSystemServiceTest {
                 .environmentDto(getEnvironment())
                 .blueprintMap(null) // needs to have at least one component, needs to have same components as blueprint in blueprint svc and all providers of component valid
                 .build();
-
-        liveSystemService.instantiate(command);
+        return command;
     }
+
 
     private EnvironmentDto getEnvironment() {
         return EnvironmentDto.builder()
