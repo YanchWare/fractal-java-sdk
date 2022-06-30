df9034f6c0148ca46111dd960c001dc6a8382713
Kamil Ulewicz <kamil@yanchware.com>
2022-06-30 09:29:59 +0200

    Add required branchName to CaaSK8sWorkloadImpl

--- fractal.sdk/src/test/java/com/yanchware/fractal/sdk/domain/entities/livesystem/caas/KubernetesWorkloadTest.java
+++ fractal.sdk/src/test/java/com/yanchware/fractal/sdk/domain/entities/livesystem/caas/KubernetesWorkloadTest.java
@@ -8,80 +8,95 @@ import static org.assertj.core.api.Assertions.*;
 
 public class KubernetesWorkloadTest {
 
-    @Test
-    public void exceptionThrown_when_workloadBuiltWithNullId() {
-        assertThatThrownBy(() -> builder().withId("").build()).
-                isInstanceOf(IllegalArgumentException.class).
-                hasMessageContaining("Component Id is illegal");
-    }
+  @Test
+  public void exceptionThrown_when_workloadBuiltWithNullId() {
+    assertThatThrownBy(() -> builder().withId("").build()).
+        isInstanceOf(IllegalArgumentException.class).
+        hasMessageContaining("Component Id is illegal");
+  }
 
-    @Test
-    public void exceptionThrown_when_workloadBuiltWithContainerPlatformBlank() {
-        assertThatThrownBy(() -> generateBuilder().withContainerPlatform("").build()).
-                isInstanceOf(IllegalArgumentException.class).
-                hasMessageContaining("ContainerPlatform is either empty or blank");
-    }
+  @Test
+  public void exceptionThrown_when_workloadBuiltWithContainerPlatformBlank() {
+    assertThatThrownBy(() -> generateBuilder().withContainerPlatform("").build()).
+        isInstanceOf(IllegalArgumentException.class).
+        hasMessageContaining("ContainerPlatform is either empty or blank");
+  }
 
-    @Test
-    public void exceptionThrown_when_workloadBuiltWithContainerPlatformEmpty() {
-        assertThatThrownBy(() -> generateBuilder().withContainerPlatform("   ").build()).
-                isInstanceOf(IllegalArgumentException.class).
-                hasMessageContaining("ContainerPlatform is either empty or blank");
-    }
+  @Test
+  public void exceptionThrown_when_workloadBuiltWithContainerPlatformEmpty() {
+    assertThatThrownBy(() -> generateBuilder().withContainerPlatform("   ").build()).
+        isInstanceOf(IllegalArgumentException.class).
+        hasMessageContaining("ContainerPlatform is either empty or blank");
+  }
 
-    @Test
-    public void exceptionThrown_when_workloadBuiltWithEmptyValues() {
-        assertThatThrownBy(() -> generateBuilder().build()).
-                isInstanceOf(IllegalArgumentException.class).
-                hasMessageContainingAll(
-                        "Namespace is either empty or blank",
-                        "privateSSHKeyPassphraseSecretId is either empty or blank",
-                        "privateSSHKeySecretId is either empty or blank",
-                        "sshRepositoryURI is either empty or blank",
-                        "repoId is either empty or blank");
-    }
+  @Test
+  public void exceptionThrown_when_workloadBuiltWithEmptyValues() {
+    assertThatThrownBy(() -> generateBuilder().build()).
+        isInstanceOf(IllegalArgumentException.class).
+        hasMessageContainingAll(
+            "Namespace is either empty or blank",
+            "privateSSHKeyPassphraseSecretId is either empty or blank",
+            "privateSSHKeySecretId is either empty or blank",
+            "sshRepositoryURI is either empty or blank",
+            "repoId is either empty or blank");
+  }
 
-    @Test
-    public void typeIsKubernetes_when_workloadBuiltWithAllRequiredValues() {
-        var builder = generateBuilder()
-                .withNamespace("fractal")
-                .withPrivateSSHKeyPassphraseSecretId("svc-private-ssh-key-pass")
-                .withPrivateSSHKeySecretId("svc-private-ssh-key-secret")
-                .withSSHRepositoryURI("ssh")
-                .withRepoId("repo-id");
-        assertThat(builder.build().getType()).isEqualTo(K8S_WORKLOAD);
-        assertThatCode(builder::build).doesNotThrowAnyException();
-    }
+  @Test
+  public void typeIsKubernetes_when_workloadBuiltWithAllRequiredValues() {
+    var builder = generateBuilder()
+        .withNamespace("fractal")
+        .withPrivateSSHKeyPassphraseSecretId("svc-private-ssh-key-pass")
+        .withPrivateSSHKeySecretId("svc-private-ssh-key-secret")
+        .withSSHRepositoryURI("ssh")
+        .withRepoId("repo-id")
+        .withBranchName("branch-name");
+    assertThat(builder.build().getType()).isEqualTo(K8S_WORKLOAD);
+    assertThatCode(builder::build).doesNotThrowAnyException();
+  }
 
-    @Test
-    public void exceptionThrown_when_workloadBuiltWithWorkloadSecretIdKeyEmpty() {
-        var builder = generateBuilder()
-            .withNamespace("fractal")
-            .withPrivateSSHKeyPassphraseSecretId("svc-private-ssh-key-pass")
-            .withPrivateSSHKeySecretId("svc-private-ssh-key-secret")
-            .withSSHRepositoryURI("ssh")
-            .withRepoId("repo-id")
-            .withSecretIdKey("");
-        assertThatThrownBy(builder::build).
-            isInstanceOf(IllegalArgumentException.class).
-            hasMessageContaining("Workload Secret Id Key is either empty or blank");
-    }
+  @Test
+  public void exceptionThrown_when_workloadBuiltWithWorkloadSecretIdKeyEmpty() {
+    var builder = generateBuilder()
+        .withNamespace("fractal")
+        .withPrivateSSHKeyPassphraseSecretId("svc-private-ssh-key-pass")
+        .withPrivateSSHKeySecretId("svc-private-ssh-key-secret")
+        .withSSHRepositoryURI("ssh")
+        .withRepoId("repo-id")
+        .withSecretIdKey("");
+    assertThatThrownBy(builder::build).
+        isInstanceOf(IllegalArgumentException.class).
+        hasMessageContaining("Workload Secret Id Key is either empty or blank");
+  }
 
-    @Test
-    public void exceptionThrown_when_workloadBuiltWithWorkloadSecretPasswordKeyEmpty() {
-        var builder = generateBuilder()
-            .withNamespace("fractal")
-            .withPrivateSSHKeyPassphraseSecretId("svc-private-ssh-key-pass")
-            .withPrivateSSHKeySecretId("svc-private-ssh-key-secret")
-            .withSSHRepositoryURI("ssh")
-            .withRepoId("repo-id")
-            .withSecretPasswordKey("");
-        assertThatThrownBy(builder::build).
-            isInstanceOf(IllegalArgumentException.class).
-            hasMessageContaining("Workload Secret Password Key is either empty or blank");
-    }
+  @Test
+  public void exceptionThrown_when_workloadBuiltWithBranchNameKeyEmpty() {
+    var builder = generateBuilder()
+        .withNamespace("fractal")
+        .withPrivateSSHKeyPassphraseSecretId("svc-private-ssh-key-pass")
+        .withPrivateSSHKeySecretId("svc-private-ssh-key-secret")
+        .withSSHRepositoryURI("ssh")
+        .withRepoId("repo-id")
+        .withBranchName("");
+    assertThatThrownBy(builder::build).
+        isInstanceOf(IllegalArgumentException.class).
+        hasMessageContaining("branchName is either empty or blank and it is required");
+  }
 
-    private KubernetesWorkloadBuilder generateBuilder() {
-        return builder().withId("kube");
-    }
+  @Test
+  public void exceptionThrown_when_workloadBuiltWithWorkloadSecretPasswordKeyEmpty() {
+    var builder = generateBuilder()
+        .withNamespace("fractal")
+        .withPrivateSSHKeyPassphraseSecretId("svc-private-ssh-key-pass")
+        .withPrivateSSHKeySecretId("svc-private-ssh-key-secret")
+        .withSSHRepositoryURI("ssh")
+        .withRepoId("repo-id")
+        .withSecretPasswordKey("");
+    assertThatThrownBy(builder::build).
+        isInstanceOf(IllegalArgumentException.class).
+        hasMessageContaining("Workload Secret Password Key is either empty or blank");
+  }
+
+  private KubernetesWorkloadBuilder generateBuilder() {
+    return builder().withId("kube");
+  }
 }
