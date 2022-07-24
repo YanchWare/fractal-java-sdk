2d1719966ae9c04087d95c1a24212a48b7d5581d
Angelo Agatino Nicolosi <angelonicolosi@yanchware.com>
2022-07-25 00:51:17 +0200

    Fixed entry points

--- fractal.sdk/src/main/java/com/yanchware/fractal/sdk/domain/entities/livesystem/caas/TraefikEntryPoint.java
+++ fractal.sdk/src/main/java/com/yanchware/fractal/sdk/domain/entities/livesystem/caas/TraefikEntryPoint.java
@@ -1,4 +1,14 @@
 package com.yanchware.fractal.sdk.domain.entities.livesystem.caas;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class TraefikEntryPoint {
+  private String name;
+  private boolean isTlsEnabled;
+  private int port;
+  private String protocol;
+  private boolean isHttpsRedirected;
 }
