3cbf59a1b8182b33250b51585734fa2ab3666a99
Kamil Ulewicz <kamil@yanchware.com>
2022-11-10 01:09:34 +0100

    change ComponentId MAX_LEN to 63 characters

--- fractal.sdk/src/main/java/com/yanchware/fractal/sdk/valueobjects/ComponentId.java
+++ fractal.sdk/src/main/java/com/yanchware/fractal/sdk/valueobjects/ComponentId.java
@@ -12,11 +12,16 @@ import static org.apache.commons.lang3.StringUtils.isBlank;
 
 @Data
 public class ComponentId {
+    
+    protected final static Integer MIN_LENGTH = 3;
+    protected final static Integer MAX_LENGTH = 63;
 
     private final static Pattern valueValidation = Pattern.compile("^(?!\\d)[a-z\\d]+((?:-[a-z\\d]+)*)+$");
     protected final static String ID_NULL_OR_EMPTY_TEMPLATE = "Component id '%s' is illegal. A valid component id cannot be null, empty or contain spaces";
     protected final static String ILLEGAL_ID_TEMPLATE = "Component id '%s' is illegal. A valid component id must start with a lowercase character and it can only contain lowercase characters, digits and hyphens";
-    protected final static String ID_LENGTH_MISMATCH_TEMPLATE = "Component id '%s' is illegal. A valid component Id must be between 3 and 15 characters of length";
+    protected final static String ID_LENGTH_MISMATCH_TEMPLATE =
+        "Component id '%s' is illegal. A valid component Id must be between " +  MIN_LENGTH + 
+            " and " + MAX_LENGTH + " characters of length";
     private final String value;
 
     protected ComponentId(String value) {
@@ -44,7 +49,8 @@ public class ComponentId {
             ret.add(String.format(ILLEGAL_ID_TEMPLATE, value));
         }
 
-        if (value.length() < 3 || value.length() > 15) {
+        var length = value.length();
+        if (length < MIN_LENGTH || length > MAX_LENGTH) {
             ret.add(String.format(ID_LENGTH_MISMATCH_TEMPLATE, value));
         }
 
