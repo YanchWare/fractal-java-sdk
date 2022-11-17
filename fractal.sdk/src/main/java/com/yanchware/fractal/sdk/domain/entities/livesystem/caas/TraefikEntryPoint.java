f0d2d663394fa4ab530b787f3632b0898a42e770
Kamil Ulewicz <kamil@yanchware.com>
2022-11-17 07:58:02 +0100

    Use proper JSON properties

--- fractal.sdk/src/main/java/com/yanchware/fractal/sdk/domain/entities/livesystem/caas/TraefikEntryPoint.java
+++ fractal.sdk/src/main/java/com/yanchware/fractal/sdk/domain/entities/livesystem/caas/TraefikEntryPoint.java
@@ -1,5 +1,6 @@
 package com.yanchware.fractal.sdk.domain.entities.livesystem.caas;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,9 +8,16 @@ import lombok.Getter;
 @AllArgsConstructor
 public class TraefikEntryPoint {
   private String name;
+
+  @JsonProperty(value="isTlsEnabled")
   private boolean isTlsEnabled;
+  
   private int port;
+  
   private int exposedPort;
+  
   private String protocol;
+  
+  @JsonProperty(value="isHttpsRedirected")
   private boolean isHttpsRedirected;
 }
