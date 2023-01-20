2f4700eee3aa0b1c090d5bca17150c44bbba700b
Project Collection Build Service (TARGITCloud) <build-service@targit.com>
2023-01-20 09:25:42 +0100

    Improve AzureMachineTypeTest

--- fractal.sdk/src/test/java/com/yanchware/fractal/sdk/domain/entities/livesystem/paas/providers/azure/AzureMachineTypeTest.java
+++ fractal.sdk/src/test/java/com/yanchware/fractal/sdk/domain/entities/livesystem/paas/providers/azure/AzureMachineTypeTest.java
@@ -1,11 +1,21 @@
 package com.yanchware.fractal.sdk.domain.entities.livesystem.paas.providers.azure;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AzureMachineTypeTest {
   @Test
+  @Order(1)
+  public void valuesContainsNewValue_when_CustomValueAdded() {
+
+    var newValue = AzureMachineType.fromString("This_Not_Exist_For_Sure");
+    assertThat(AzureMachineType.values()).contains(newValue);
+  }
+  
+  @Test
+  @Order(2)
   public void correctValues_when_customEnum() {
     var valueToExtend = "This_Not_Exist_For_Sure";
     var enumValue = AzureMachineType.fromString(valueToExtend);
@@ -14,21 +24,11 @@ public class AzureMachineTypeTest {
   }
 
   @Test
+  @Order(3)
   public void correctValues_when_Enum() {
     var existingEnum = "Basic_A0";
     var enumValue = AzureMachineType.fromString(existingEnum);
 
     assertThat(existingEnum).isEqualTo(enumValue.toString());
-  }
-
-  @Test
-  public void valuesContainsNewValue_when_CustomValueAdded() {
-    var initialValues = AzureMachineType.values();
-    
-    var newValue = AzureMachineType.fromString("This_Not_Exist_For_Sure");
-
-    var valuesWithNewValue = AzureMachineType.values();
-    assertThat(valuesWithNewValue).contains(newValue);
-    assertThat(valuesWithNewValue.size()).isEqualTo(initialValues.size() + 1);
   }
 }
