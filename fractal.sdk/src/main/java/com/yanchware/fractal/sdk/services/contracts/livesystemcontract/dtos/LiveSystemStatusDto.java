31b0c194e44b44eb3203b44e30e71acca0d044cd
Project Collection Build Service (TARGITCloud) <build-service@targit.com>
2023-04-18 07:55:25 +0200

    Update LiveSystemStatusDto enum

--- fractal.sdk/src/main/java/com/yanchware/fractal/sdk/services/contracts/livesystemcontract/dtos/LiveSystemStatusDto.java
+++ fractal.sdk/src/main/java/com/yanchware/fractal/sdk/services/contracts/livesystemcontract/dtos/LiveSystemStatusDto.java
@@ -1,12 +1,12 @@
 package com.yanchware.fractal.sdk.services.contracts.livesystemcontract.dtos;
 
 public enum LiveSystemStatusDto {
-  UNKNOWN,
-  MUTATING,
-  ACTIVE,
-  FAILEDMUTATION,
-  PROCESSING,
-  ERROR,
-  READY,
-  DELETING
+  Unknown,
+  Instantiating,
+  Active,
+  Failed,
+  Mutating,
+  Deleting,
+  Deleted,
+  Cancelled
 }
