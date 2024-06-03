66907117d31bdb2cff5e1eeb85ee2e6dce4e5db8
Angelo Agatino Nicolosi <aan@yanchware.com>
2024-06-03 08:53:26 +0200

    Fixing issue with compartments

--- fractal.sdk/src/main/java/com/yanchware/fractal/sdk/domain/entities/livesystem/paas/providers/azure/AzureResourceEntity.java
+++ fractal.sdk/src/main/java/com/yanchware/fractal/sdk/domain/entities/livesystem/paas/providers/azure/AzureResourceEntity.java
@@ -1,20 +1,16 @@
 package com.yanchware.fractal.sdk.domain.entities.livesystem.paas.providers.azure;
 
+import com.yanchware.fractal.sdk.domain.entities.livesystem.paas.providers.ResourceEntity;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
-public interface AzureResourceEntity {
+public interface AzureResourceEntity extends ResourceEntity {
   AzureResourceGroup getAzureResourceGroup();
   void setAzureResourceGroup(AzureResourceGroup azureResourceGroup);
   AzureRegion getAzureRegion();
   void setAzureRegion(AzureRegion region);
-
-  Map<String, String> getTags();
-  void setTags(Map<String, String> tags);
-
-  String getName();
-  void setName(String Name);
 
   static Collection<String> validateAzureResourceEntity(AzureResourceEntity resourceEntity, String entityName) {
     final var REGION_IS_NULL_TEMPLATE = "[Azure %s Validation] Region has not been defined and it is required";
--- fractal.sdk/src/main/java/com/yanchware/fractal/sdk/services/contracts/livesystemcontract/dtos/ProviderType.java
+++ fractal.sdk/src/main/java/com/yanchware/fractal/sdk/services/contracts/livesystemcontract/dtos/ProviderType.java
@@ -5,5 +5,6 @@ public enum ProviderType {
     GCP,
     AZURE,
     CAAS,
-    SAAS
+    SAAS,
+    OCI
 }
