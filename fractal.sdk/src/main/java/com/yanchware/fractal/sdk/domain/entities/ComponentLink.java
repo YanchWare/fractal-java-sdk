699f8325bc36cb2d3ab45c54f65107d62576b61b
Kamil Ulewicz <kamil@yanchware.com>
2024-06-10 09:28:25 +0200

    Address PR comments

--- fractal.sdk/src/main/java/com/yanchware/fractal/sdk/domain/entities/ComponentLink.java
+++ fractal.sdk/src/main/java/com/yanchware/fractal/sdk/domain/entities/ComponentLink.java
@@ -12,81 +12,77 @@ import java.util.*;
 @Setter(AccessLevel.PRIVATE)
 @ToString
 public class ComponentLink implements Validatable {
-    private static final String ROLE_NAME_PARAM_KEY = "roleName";
-    
-    public String componentId;
-    public Map<String, Object> settings;
+  private static final String ROLE_NAME_PARAM_KEY = "roleName";
 
-    public ComponentLink() {
-        settings = new HashMap<>();
+  public String componentId;
+  public Map<String, Object> settings;
+
+  public ComponentLink() {
+    settings = new HashMap<>();
+  }
+
+  public static ComponentLinkBuilder builder() {
+    return new ComponentLinkBuilder();
+  }
+
+  public static class ComponentLinkBuilder {
+    private ComponentLink componentLink;
+    private ComponentLinkBuilder builder;
+
+    public ComponentLinkBuilder() {
+      componentLink = createComponentLink();
+      builder = getBuilder();
     }
 
-    public static ComponentLinkBuilder builder() {
-        return new ComponentLinkBuilder();
+    protected ComponentLink createComponentLink() {
+      return new ComponentLink();
     }
 
-    public static class ComponentLinkBuilder {
-        private ComponentLink componentLink;
-        private ComponentLinkBuilder builder;
-
-        public ComponentLinkBuilder() {
-            componentLink = createComponentLink();
-            builder = getBuilder();
-        }
-
-        protected ComponentLink createComponentLink() {
-            return new ComponentLink();
-        }
-
-        protected ComponentLinkBuilder getBuilder() {
-            return this;
-        }
-
-        public ComponentLinkBuilder withComponentId(Component component) {
-            componentLink.setComponentId(component.getId().getValue());
-            return builder;
-        }
-
-        public ComponentLinkBuilder withComponentId(ComponentId componentId) {
-            componentLink.setComponentId(componentId.getValue());
-            return builder;
-        }
-
-        public ComponentLinkBuilder withComponentId(String componentId) {
-            componentLink.setComponentId(componentId);
-            return builder;
-        }
-
-        public ComponentLinkBuilder withSettings(Map<String, Object> settings) {
-            componentLink.setSettings(new HashMap<>(settings));
-            return builder;
-        }
-
-        public ComponentLinkBuilder withRoleName(String roleName) {
-            if (componentLink.settings.containsKey(ROLE_NAME_PARAM_KEY)) {
-                componentLink.settings.replace(ROLE_NAME_PARAM_KEY, roleName);
-            } else {
-                componentLink.getSettings().put(ROLE_NAME_PARAM_KEY, roleName);
-            }
-
-            return builder;
-        }
-
-        public ComponentLink build() {
-            Collection<String> errors = componentLink.validate();
-
-            if (!errors.isEmpty()) {
-                throw new IllegalArgumentException(String.format(
-                        "ComponentLink validation failed. Errors: %s",
-                        Arrays.toString(errors.toArray())));
-            }
-
-            return componentLink;
-        }
+    protected ComponentLinkBuilder getBuilder() {
+      return this;
     }
 
-    @Override
-    public Collection<String> validate() {
-        return ComponentId.validate(componentId);
+    public ComponentLinkBuilder withComponentId(Component component) {
+      componentLink.setComponentId(component.getId().getValue());
+      return builder;
     }
+
+    public ComponentLinkBuilder withComponentId(ComponentId componentId) {
+      componentLink.setComponentId(componentId.getValue());
+      return builder;
+    }
+
+    public ComponentLinkBuilder withComponentId(String componentId) {
+      componentLink.setComponentId(componentId);
+      return builder;
+    }
+
+    public ComponentLinkBuilder withSettings(Map<String, Object> settings) {
+      componentLink.settings.putAll(settings);
+      return builder;
+    }
+
+    public ComponentLinkBuilder withRoleName(String roleName) {
+      componentLink.settings.put(ROLE_NAME_PARAM_KEY, roleName);
+
+      return builder;
+    }
+
+    public ComponentLink build() {
+      Collection<String> errors = componentLink.validate();
+
+      if (!errors.isEmpty()) {
+        throw new IllegalArgumentException(String.format(
+            "ComponentLink validation failed. Errors: %s",
+            Arrays.toString(errors.toArray())));
+      }
+
+      return componentLink;
+    }
+  }
+
+  @Override
+  public Collection<String> validate() {
+    return ComponentId.validate(componentId);
+  }
 }
