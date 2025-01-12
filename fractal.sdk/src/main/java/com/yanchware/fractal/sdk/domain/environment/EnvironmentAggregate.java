1a3632d26d892dd883e5a60aa57de3b31866326f
Angelo <angelonicolosi@yanchware.com>
2025-01-12 13:27:50 +0100

    Fix secret handling (#196)
    * Fix secret handling
    
    * Fix secret handling
    
    * Simplify test

--- fractal.sdk/src/main/java/com/yanchware/fractal/sdk/domain/environment/EnvironmentAggregate.java
+++ fractal.sdk/src/main/java/com/yanchware/fractal/sdk/domain/environment/EnvironmentAggregate.java
@@ -1,18 +1,16 @@
 package com.yanchware.fractal.sdk.domain.environment;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.yanchware.fractal.sdk.configuration.SdkConfiguration;
+import com.yanchware.fractal.sdk.domain.environment.service.EnvironmentService;
 import com.yanchware.fractal.sdk.domain.environment.service.EnvironmentService;
 import com.yanchware.fractal.sdk.domain.environment.service.dtos.EnvironmentResponse;
 import com.yanchware.fractal.sdk.domain.exceptions.InstantiatorException;
 import com.yanchware.fractal.sdk.utils.SerializationUtils;
-import io.github.resilience4j.retry.RetryRegistry;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
-import java.net.http.HttpClient;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -27,16 +25,12 @@ public class EnvironmentAggregate {
   @Getter
   private ManagementEnvironment managementEnvironment;
 
-  public EnvironmentAggregate(
-      HttpClient client,
-      SdkConfiguration sdkConfiguration,
-      RetryRegistry retryRegistry) {
-    this.service = new EnvironmentService(client, sdkConfiguration, retryRegistry);
+  public EnvironmentAggregate(EnvironmentService environmentService) {
+    this.service = environmentService;
   }
 
   public void createOrUpdate() throws InstantiatorException {
     var managementEnvironmentId = managementEnvironment.getId();
-
 
     var existingEnvironment = service.fetch(managementEnvironmentId);
     if (existingEnvironment != null) {
@@ -74,19 +68,14 @@ public class EnvironmentAggregate {
   }
 
   private void manageEnvironmentSecrets(EnvironmentIdValue environmentId, Collection<Secret> environmentSecrets) throws InstantiatorException {
-    var existingSecrets = fetchExistingSecrets(environmentId);
+    var existingSecrets = fetchExistingSecretIds(environmentId);
     
     for (var secret : environmentSecrets) {
       var secretName = secret.name();
       var secretValue = secret.value();
 
-      if (existingSecrets.containsKey(secretName)) {
-        if (!existingSecrets.get(secretName).equals(secretValue)) {
-          log.info("Updating secret [name: '{}', environmentId: '{}']", secretName, environmentId);
-          service.updateSecret(environmentId, secretName, secretValue);
-        } else {
-          log.info("Secret is up-to-date [name: '{}', environmentId: '{}']", secretName, environmentId);
-        }
+      if (existingSecrets.contains(secretName)) {
+        service.updateSecret(environmentId, secretName, secretValue);
         existingSecrets.remove(secretName);
       } else {
         log.info("Creating secret [name: '{}', environmentId: '{}']", secretName, environmentId);
@@ -94,24 +83,22 @@ public class EnvironmentAggregate {
       }
     }
     
-    for (String secretName : existingSecrets.keySet()) {
-      log.info("Deleting secret [name: '{}', environmentId: '{}']", secretName, environmentId);
-
+    for (String secretName : existingSecrets) {
+      log.info("Deleting secret [name: '{}', environmentId: '{}'] as it is not present any longer on the live environment definition", secretName, environmentId);
       service.deleteSecret(environmentId, secretName);
     }
   }
 
-  private Map<String, String> fetchExistingSecrets(EnvironmentIdValue environmentId) throws InstantiatorException {
-    Map<String, String> existingSecrets = new HashMap<>();
+  private List<String> fetchExistingSecretIds(EnvironmentIdValue environmentId) throws InstantiatorException {
+    List<String> existingSecrets = new ArrayList<>();
 
     var secrets = service.getSecrets(environmentId);
 
     if (secrets != null) {
       for (var secret : secrets) {
-        existingSecrets.put(secret.name(), secret.value());
+        existingSecrets.add(secret.name());
       }
     }
-
     return existingSecrets;
   }
 
