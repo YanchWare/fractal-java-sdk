6a1ae059096939c5d5be1fac93c35f96978c6157
CodeCloud <81808382+CodeCloudKU@users.noreply.github.com>
2025-02-27 12:35:45 +0100

    Throw Exception when wrong input in EnvironmentType.fromString (#206)
    * Throw Exception when wrong input in EnvironmentType.fromString
    
    * Add Allowed values to exception

--- fractal.sdk/src/main/java/com/yanchware/fractal/sdk/domain/environment/EnvironmentType.java
+++ fractal.sdk/src/main/java/com/yanchware/fractal/sdk/domain/environment/EnvironmentType.java
@@ -2,6 +2,10 @@ package com.yanchware.fractal.sdk.domain.environment;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 
+import java.util.Arrays;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 public enum EnvironmentType {
   PERSONAL("Personal"),
   ORGANIZATIONAL("Organizational");
@@ -18,12 +22,21 @@ public enum EnvironmentType {
   }
 
   public static EnvironmentType fromString(String text) {
+    if(isBlank(text)) {
+      throw new IllegalArgumentException(
+              String.format("Environment type cannot be null or blank. Allowed values: %s", 
+                      Arrays.toString(EnvironmentType.values())));
+    }
+
     for (var item : EnvironmentType.values()) {
       if (item.environmentType.equalsIgnoreCase(text)) {
         return item;
       }
     }
-    return null;
+
+    throw new IllegalArgumentException(
+            String.format("Invalid environment type: '%s'. Allowed values: %s", 
+                    text, Arrays.toString(EnvironmentType.values())));
   }
 
   @Override
